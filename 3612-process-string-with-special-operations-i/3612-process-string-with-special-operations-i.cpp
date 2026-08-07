efe342a9eb3bb7fbf0d2effba3838c52838983d8
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-07 23:32:02 +0530

    Time: 0 ms (100%), Space: 39.7 MB (71.86%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '*') {
+                if (!result.empty())
+                    result.pop_back();
+            }
+            else if (s[i] == '#') {
+                result += result;
+            }
+            else if (s[i] == '%') {
+                reverse(result.begin(), result.end());
+            }
+            else {
+                result += s[i];
+            }
+        }
+
+        return result;
+    }
+};
