88ce9b655aa20893a74d84483638d8dfd712106b
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-11 21:01:37 +0530

    Time: 1 ms (23.43%), Space: 9.4 MB (15.98%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    int strStr(string haystack, string needle) {
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        if (m == 0)
+            return 0;
+
+        // Build LPS
+        vector<int> lps(m, 0);
+
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        // KMP search
+        i = 0;
+        int j = 0;
+
+        while (i < n) {
+
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+
+                // Complete needle found
+                if (j == m) {
+                    return i - m;
+                }
+            }
+            else {
+
+                if (j != 0) {
+                    j = lps[j - 1];
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
