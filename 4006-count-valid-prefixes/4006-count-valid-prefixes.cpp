5004ef9927da9cb15f04919aff448e3ccf4ce331
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-12 23:37:33 +0530

    Time: 3 ms (21.32%), Space: 9.5 MB (59.59%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int count0=0,count1=0,ans=0;
+            for(int i=0;i<s.size();i++){
+                if(s[i]=='0'){
+                    count0++;
+                }
+                else{
+                    count1++;
+                }
+                if(abs(count0-count1)<=1){
+                    ans++;
+                }
+
+            }
+            return ans;
+        }
+    };
