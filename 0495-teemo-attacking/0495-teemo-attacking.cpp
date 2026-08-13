49fed74e1058b69bebab1da8c759ac96f14f2584
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-14 00:24:04 +0530

    Time: 0 ms (100%), Space: 29.7 MB (30.82%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        int ans=0;
-        for(int i=0;i<timeSeries.size()-1;i++){
-           int gap=timeSeries[i+1]-timeSeries[i];
-           ans+=min(gap,duration);
+        // int ans=0;
+        // for(int i=0;i<timeSeries.size()-1;i++){
+        //    int gap=timeSeries[i+1]-timeSeries[i];
+        //    ans+=min(gap,duration);
+        // }
+        // ans+=duration;
+        // return ans;
+        long long sum=0;
+        for(int i=0;i<timeSeries.size();i++){
+            sum+=duration;
         }
-        ans+=duration;
-        return ans;
+        for(int i=0;i<timeSeries.size()-1;i++){
+            if(timeSeries[i+1]-timeSeries[i]<duration){
+                sum-=duration-(timeSeries[i+1]-timeSeries[i]);
+            }
+        }
+        return sum;
     }
 };
