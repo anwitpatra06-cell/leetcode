3f34ed350f0cb59977b4c393593ccef329f5816a
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-15 00:14:31 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.19%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int last = nums.size() - 1;
+        int farthest = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > farthest) {
+                return false;
+            }
+            farthest = max(farthest, i + nums[i]);
+            if (farthest >= last) {
+                return true;
+            }
+        }
+        return true;
+    }
+};
