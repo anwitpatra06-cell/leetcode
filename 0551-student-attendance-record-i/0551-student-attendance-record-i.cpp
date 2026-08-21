c11656d8b2b4bf00b055dcebe563a732c28da39d
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-22 02:57:10 +0530

    Time: 2 ms (3.9%), Space: 8.4 MB (54.19%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -11,27 +11,7 @@ public:
         if(counta<2){
             a=true;
         }
-        // for(int i=0;i<s.size();i++){
-        //     if(s[i]=='L'){
-        //         for(int j=0;j<3;j++){
-        //             if(s[j]!='L'){
-        //                 l=true;
-        //             }
-        //         }
-        //     }
-        // }
-        // int j=0;
-        // while(j<s.size()){
-        //     if(s[j]=='L'){
-        //         for(int i=j+1;i<=j+2;i++){
-        //             if(s[i]!='L'){
-        //                 l=true;
-        //             }
-        //         }
-        //         j+=2;
-        //     }
-        //   j++;  
-        // }
+
 
 if (s.find("LLL") != string::npos){
     l=false;
