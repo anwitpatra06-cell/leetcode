8797eaadd8ed1031dd357939b24950c22973b1bb
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-08-27 19:58:52 +0530

    Time: 0 ms (100%), Space: 108.1 MB (43.83%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int maxi1=INT_MIN;
-        int mini1=INT_MAX;
-         int maxi2=INT_MIN;
-        int mini2=INT_MAX;
-        int idx=-1;
-        for(int i = 0; i < arrays.size(); i++){
-            if(arrays[i].back()>maxi1){
-                maxi1=arrays[i].back();
-                idx=i;
-            }
-            
-        }
-        for(int i = 0; i < arrays.size(); i++){
-            if(i==idx)  continue;
-            mini1=min(arrays[i].front(),mini1);
-        }
-        int ans1=(maxi1-mini1);
-        for(int i = 0; i < arrays.size(); i++){
-            if(arrays[i].front()<mini2){
-                mini2=arrays[i].front();
-                idx=i;
-            }
-            
-        }
-        for(int i = 0; i < arrays.size(); i++){
-            if(i==idx)  continue;
-            maxi2=max(arrays[i].back(),maxi2);
+        int mn = arrays[0][0];
+        int mx = arrays[0].back();
+        int ans = 0;
+
+        for(int i = 1; i < arrays.size(); i++) {
+            ans = max(ans, arrays[i].back() - mn);
+            ans = max(ans, mx - arrays[i][0]);
+
+            mn = min(mn, arrays[i][0]);
+            mx = max(mx, arrays[i].back());
         }
 
-        int ans2=(maxi2-mini2);
-        return max(ans1,ans2);
+        return ans;
     }
 };
