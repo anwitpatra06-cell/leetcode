335e09ea43dc9785a00686f207dceb657d520ec9
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-09-18 21:50:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (84.4%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int n=s.size();
+        int one=0,zero=0,ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                one++;
+            }
+        }
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='0'){
+                zero++;
+            }
+            else{
+                one--;
+            }
+            ans=max(ans,one+zero);
+        }
+        return ans;
+    }
+};
