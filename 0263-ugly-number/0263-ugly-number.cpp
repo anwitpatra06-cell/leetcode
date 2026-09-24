de557a1f395c6da6daaeff86d053533062fb8f2d
anwitpatra06-cell <anwitpatra06@gmail.com>
2026-09-24 12:00:51 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.16%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if (n <= 0) return false;
-
-        for (long long i = 2; i * i <= n; i++) {
-            while (n % i == 0) {
-                if (i != 2 && i != 3 && i != 5)
-                    return false;
-
-                n /= i;
-            }
-        }
-
-        if (n > 1 && n != 2 && n != 3 && n != 5)
+        if (n <= 0)
             return false;
 
-        return true;
+        while (n % 2 == 0)
+            n /= 2;
+
+        while (n % 3 == 0)
+            n /= 3;
+
+        while (n % 5 == 0)
+            n /= 5;
+
+        return n == 1;
     }
 };
